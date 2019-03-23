c55531cbd52b063dbebacdc3cb6bdffcd1d2a8ad
shuyueCao <141250005@smail.nju.edu.cn>
2019-03-23 10:52:09 +0800

    create

---
+++ src/main/java/com/example/fightinggame/FightinggameApplication.java
@@ -0,0 +1,13 @@
+package com.example.fightinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightinggameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FightinggameApplication.class, args);
+    }
+
+}
